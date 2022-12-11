e1f7e959a0d162c88e7f740f3f00016ec37b2bbd
achiez <106531132+achiez@users.noreply.github.com>
2022-12-11 06:06:23 +0200

    Delete TestConsole directory

--- TestConsole/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Specialized;
-using System.Diagnostics;
-using System.Reflection.Metadata;
-using System.Security.AccessControl;
-using AchiesUtilities;
-using AchiesUtilities.Collections;
-
-
-
