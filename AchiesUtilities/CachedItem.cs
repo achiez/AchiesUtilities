f133502daa33f69b0222d84c7c50df7cfea5c148
achiez <106531132+achiez@users.noreply.github.com>
2022-12-16 20:41:13 +0200

    cached item

---
+++ AchiesUtilities/CachedItem.cs
@@ -0,0 +1,28 @@
+﻿namespace AchiesUtilities;
+
+public class CachedItem<T>
+{
+    public T Value { get; }
+    public DateTime CachedAt { get; set; }
+    public DateTime ExpiresAt { get; }
+    public bool Expired
+    {
+        get => _expired || DateTime.Now > ExpiresAt;
+        set => _expired = value;
+    }
+    private bool _expired = false;
+
+    public CachedItem(T item)
+    {
+        Value = item;
+        ExpiresAt = DateTime.MaxValue;
+    }
+
+    public CachedItem(T item, DateTime expiresAt, DateTime? cachedAt = null)
+    {
+        Value = item;
+        ExpiresAt = expiresAt;
+        CachedAt = cachedAt ?? DateTime.Now;
+    }
+
+}
