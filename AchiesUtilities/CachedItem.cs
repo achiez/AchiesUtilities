395677d752c9224a2c5634af0cfc4a3e1842be1b
achiez <vectisraw@gmail.com>
2023-01-07 21:57:09 +0200

    cached item become struct

--- AchiesUtilities/CachedItem.cs
+++ AchiesUtilities/CachedItem.cs
@@ -3,10 +3,10 @@
 namespace AchiesUtilities;
 
 [PublicAPI]
-public class CachedItem<T>
+public struct CachedItem<T>
 {
     public T Value { get; }
-    public DateTime CachedAt { get; set; }
+    public DateTime CachedAt { get; }
     public DateTime ExpiresAt { get; }
     public bool Expired
     {
@@ -16,15 +16,29 @@ public class CachedItem<T>
     private bool _expired = false;
 
     public CachedItem(T item) : this(item, expiresAt: DateTime.MaxValue)
-    {
-        Value = item;
-        ExpiresAt = DateTime.MaxValue;
-    }
+    { }
 
     public CachedItem(T item, DateTime expiresAt, DateTime? cachedAt = null)
     {
         Value = item;
         ExpiresAt = expiresAt;
         CachedAt = cachedAt ?? DateTime.Now;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is CachedItem<T> i && Equals(i);
+    }
+
+    public bool Equals(CachedItem<T> other)
+    {
+        return _expired == other._expired 
+               && EqualityComparer<T>.Default.Equals(Value, other.Value) 
+               && CachedAt.Equals(other.CachedAt) 
+               && ExpiresAt.Equals(other.ExpiresAt);
+    }
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(_expired, Value, CachedAt, ExpiresAt);
     }
 }
