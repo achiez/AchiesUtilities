e95af8080917159d6ebb4e9517354f9b51329b10
achiez <vectisraw@gmail.com>
2024-01-08 00:24:53 +0200

    json.net version updated

--- AchiesUtilities/UtilitiesConfiguration.cs
+++ AchiesUtilities/UtilitiesConfiguration.cs
@@ -4,9 +4,4 @@ namespace AchiesUtilities;
 
 public static class UtilitiesConfiguration
 {
-    /// <summary>
-    /// Default value - <b>100</b> ms
-    /// </summary>
-    public static readonly SetOnceProperty<int> DefaultPauseTokenDelay = new(100);
-
 }
