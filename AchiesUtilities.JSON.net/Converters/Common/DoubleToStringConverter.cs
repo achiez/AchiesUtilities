3835efcf12d099c699c072c40620ad08c93ff8bb
achiez <vectisraw@gmail.com>
2024-01-08 00:14:22 +0200

    double to string converter added 1.1.9

---
+++ AchiesUtilities.JSON.net/Converters/Common/DoubleToStringConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using Newtonsoft.Json;
+
+namespace SteamLib.Web.Converters;
+
+public class StringToDoubleConverter : JsonConverter<double>
+{
+    public override void WriteJson(JsonWriter writer, double value, JsonSerializer serializer)
+    {
+        writer.WriteValue(value.ToString());
+    }
+
+    public override double ReadJson(JsonReader reader, Type objectType, double existingValue, bool hasExistingValue,
+        JsonSerializer serializer)
+    {
+       var value = (string)reader.Value!;
+       return double.Parse(value, CultureInfo.InvariantCulture);
+    }
+}
