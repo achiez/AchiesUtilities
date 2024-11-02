5bcc646dc078ef4b48903d500a3d86994713c162
Давид Чернопятов <vectisraw@gmail.com>
2024-11-02 14:24:04 +0200

    1.2.2 AchiesUtilites - Added ExpirationWatch

---
+++ AchiesUtilities/Models/ExpirationWatch.cs
@@ -0,0 +1,27 @@
+﻿using JetBrains.Annotations;
+
+namespace AchiesUtilities.Models;
+
+[PublicAPI]
+public struct ExpirationWatch
+{
+    public DateTime Start { get; }
+    public DateTime End { get; }
+    public ExpirationWatch(TimeSpan expiration)
+    {
+        Start = DateTime.Now;
+        End = Start + expiration;
+    }
+
+    public ExpirationWatch(DateTime start, TimeSpan expiration)
+    {
+        Start = start;
+        End = start + expiration;
+    }
+
+
+    public bool IsExpired()
+    {
+        return End < DateTime.Now;
+    }
+}
