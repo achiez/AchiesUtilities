cd2080e9ef0e1c80043b759919cfebf62814a92a
achiez <achiesraw@gmail.com>
2025-05-31 13:12:29 +0300

    1.3.3 JSON
    Fixed static initialization logic in StaticConverters. Previously, ReSharper code cleanup reordered properties, causing AllConvertersProperties to be assigned null.

--- AchiesUtilities.JSON.net/StaticConverters.cs
+++ AchiesUtilities.JSON.net/StaticConverters.cs
@@ -8,14 +8,9 @@ public static class StaticConverters
 {
     /// <summary>
     ///     Include all defaults instances of <see cref="JsonConverter" />:<br />
-    ///     <see cref="TimeSpanConverter" /><br /><see cref="UnixTimeStampConverterSeconds" />
+    ///     <see cref="TimeSpanConverter" /><br /><see cref="UnixTimeStampConverterSeconds" /><br/><see cref="NullableUnixTimeStampConverterSeconds"/>
     /// </summary>
-    public static IList<JsonConverter> AllDefaultConverters { get; } = new JsonConverter[]
-    {
-        TimeSpanConverter,
-        UnixTimeStampConverterSeconds,
-        NullableUnixTimeStampConverterSeconds
-    };
+    public static IList<JsonConverter> AllDefaultConverters { get; }
 
     #region Default
 
@@ -25,10 +20,21 @@ public static class StaticConverters
 
     #endregion
 
+
     #region Special
 
     public static BoolToIntConverter BoolToIntConverter { get; } = new();
     public static JsonTimeSpanTypedConverter TimeSpanTypedConverter { get; } = new();
 
     #endregion
+
+    static StaticConverters()
+    {
+        AllDefaultConverters = 
+        [
+            TimeSpanConverter,
+            UnixTimeStampConverterSeconds,
+            NullableUnixTimeStampConverterSeconds
+        ];
+    }
 }
